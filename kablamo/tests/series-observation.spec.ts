4fb7e934a45dba4e26ca2a0ef87d5d4ae39ff057
Austin Tam <austin.tam7@gmail.com>
2024-08-26 02:29:54 -0400

    add parameterization to test for multiple currencies

--- kablamo/tests/series-observation.spec.ts
+++ kablamo/tests/series-observation.spec.ts
@@ -1,25 +1,36 @@
 import { test, expect } from '@playwright/test'
 import Ajv from 'ajv'
 
-const CONVERTFROM = 'CAD'
-const CONVERTTO = 'AUD'
-
 test.describe('Series by observation tests', () => {
-  const ajv = new Ajv()
+  const ajv = new Ajv();
 
-  test('should successfully retrieve Forex conversion rate "CAD to AUD" for the recent 10 weeks using observations by Series', async ({ request }) => {
-    const response = await (request.get(`observations/FX${CONVERTFROM}${CONVERTTO}/json?recent_weeks=10`))
-    const validate = ajv.validate(require('./series-observation-success.schema.json'), response.body())
+  [
+    { convertFrom: 'CAD', convertTo: 'AUD' },
+    { convertFrom: 'USD', convertTo: 'CAD' },
+    { convertFrom: 'CAD', convertTo: 'JPY' },
+  ].forEach(({convertFrom, convertTo}) => {
+    test(`should successfully retrieve Forex conversion rate "${convertFrom} to ${convertTo}" for the recent 10 weeks using observations by Series`, async ({ request }) => {
+      const response = await (request.get(`observations/FX${convertFrom}${convertTo}/json?recent_weeks=10`))
+      const validate = ajv.validate(require('./series-observation-success.schema.json'), response.body())
 
-    expect(response.status()).toEqual(200)
-    expect(validate).toBe(true)
-  })
+      expect(response.status()).toEqual(200)
+      expect(validate).toBe(true)
+    })
+  });
 
   test('should return error if Forex series not found or not available', async ({ request }) => {
-    const response = await (request.get(`observations/FXUSDAUD/json?recent_weeks=10`))
+    const response = await (request.get('observations/FXUSDAUD/json?recent_weeks=10'))
     const validate = ajv.validate(require('./series-observation-error.schema.json'), response.body())
 
     expect(response.status()).toEqual(404)
+    expect(validate).toBe(true)
+  });
+
+  test('should return error if recent weeks is not numeric', async ({ request }) => {
+    const response = await (request.get('observations/FXCADAUD/json?recent_weeks=a'))
+    const validate = ajv.validate(require('./series-observation-error.schema.json'), response.body())
+
+    expect(response.status()).toEqual(400)
     expect(validate).toBe(true)
   })
 })
